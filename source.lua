d82e7a1d54c38a24d957d907f324e93c3aeee7b2
TheXPredator2009 <102040268+TheXPredator2009@users.noreply.github.com>
2024-04-10 11:22:38 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -2,25 +2,6 @@ local plr = game.Players.LocalPlayer
 local localplayer = plr
 local mouse = plr:GetMouse()
 
-if workspace:FindFirstChild("Core") then
-    workspace.Core:Destroy()
-end
-
-local Core = Instance.new("Part")
-Core.Name = "Core"
-Core.Size = Vector3.new(0.05, 0.05, 0.05)
-
-spawn(function()
-    Core.Parent = workspace
-    local Weld = Instance.new("Weld", Core)
-    Weld.Part0 = Core
-    Weld.Part1 = localplayer.Character.LowerTorso
-    Weld.C0 = CFrame.new(0, 0, 0)
-end)
-
-workspace:WaitForChild("Core")
-
-local torso = workspace.Core
 local flying = false -- Initially set to false
 local keys = {a = false, d = false, w = false, s = false}
 local e1
@@ -28,26 +9,25 @@ local e2
 
 local function start(speed)
     flying = true -- Set flying to true when start function is called
+    local torso = localplayer.Character:WaitForChild("HumanoidRootPart")
     local pos = Instance.new("BodyPosition", torso)
     local gyro = Instance.new("BodyGyro", torso)
-    pos.Name = "EPIXPOS"
     pos.MaxForce = Vector3.new(math.huge, math.huge, math.huge)
     pos.Position = torso.Position
     gyro.MaxTorque = Vector3.new(9e9, 9e9, 9e9)
     gyro.CFrame = torso.CFrame
     repeat
         wait()
-        localplayer.Character.Humanoid.PlatformStand = true
         local new = gyro.CFrame - gyro.CFrame.p + pos.Position
         if not keys.w and not keys.s and not keys.a and not keys.d then
             speed = 5
         end
         if keys.w then
-            new = new + workspace.CurrentCamera.CoordinateFrame.LookVector * speed
+            new = new + workspace.CurrentCamera.CoordinateFrame.lookVector * speed
             speed = speed + 0
         end
         if keys.s then
-            new = new - workspace.CurrentCamera.CoordinateFrame.LookVector * speed
+            new = new - workspace.CurrentCamera.CoordinateFrame.lookVector * speed
             speed = speed + 0
         end
         if keys.d then
@@ -70,13 +50,8 @@ local function start(speed)
             gyro.CFrame = workspace.CurrentCamera.CoordinateFrame
         end
     until not flying -- Stop flying when 'flying' becomes false
-    if gyro then
-        gyro:Destroy()
-    end
-    if pos then
-        pos:Destroy()
-    end
-    localplayer.Character.Humanoid.PlatformStand = false
+    pos:Destroy()
+    gyro:Destroy()
 end
 
 local function stopFlying()
@@ -84,12 +59,6 @@ local function stopFlying()
 end
 
 e1 = mouse.KeyDown:Connect(function(key)
-    if not torso or not torso.Parent then
-        flying = false
-        e1:Disconnect()
-        e2:Disconnect()
-        return
-    end
     if key == "w" then
         keys.w = true
     elseif key == "s" then
@@ -113,6 +82,4 @@ e2 = mouse.KeyUp:Connect(function(key)
     end
 end)
 
-start(5)
-
-return stopFlying -- Return function to stop flying
+return start, stopFlying -- Return functions to start and stop flying
